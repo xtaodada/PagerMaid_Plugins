c8bb61e86d2ada1fbba359d37d5dddfded2178d2
Moriarty-989 <69113033+Moriarty-989@users.noreply.github.com>
2021-02-02 14:13:48 +0800

    更换了api源，请求变快了，低端vps也减少了卡机率

--- cosplay-multi.py
+++ cosplay-multi.py
@@ -10,23 +10,17 @@ from os import remove
 async def joke(context):
     await context.edit("获取中 . . .")
     status = False
-    for _ in range (20): #最多重试20次
+    for _ in range (5): #最多重试5次
         website = randint(0, 6)
-        filename = "cosm" + str(random())[2:] + ".png"
+        filename = str(random()) + ".png"
         try:
             if website == 0:
-                img = get("https://api.helloworld.la/xiezhen_cosplay.php")
+                img = get("https://cdn.seovx.com/?mom=302")
             elif website == 1:
-                img = get("https://api.vvhan.com/api/mobil.girl?type=json")
+                img = get("https://api.ixxcc.com/cosplay.php?return=img")
             elif website == 2:
-                img = get("https://api.helloworld.la/xiezhen_weimei.php")
+                img = get("https://api.helloworld.la/xiezhen_weimei.php", timeout=5)
             elif website == 3:
-                img = get("http://api.rosysun.cn/cos")
-            elif website == 4:
-                img = get("https://uploadbeta.com/api/pictures/random/?key=%E5%8A%A8%E6%BC%AB")
-            elif website == 5:
-                img = get("https://uploadbeta.com/api/pictures/random/?key=%E4%BA%8C%E6%AC%A1%E5%85%83")
-            elif website == 6:
                 img = get("https://xn--wcs142h.herokuapp.com/")
             if img.status_code == 200:
                 if website == 3:
@@ -34,32 +28,23 @@ async def joke(context):
                     if img.status_code != 200:
                         continue #再试一次
                 with open(filename, 'wb') as f:
+                    await context.edit("上传中 . . .")
                     f.write(img.content)
-                await context.edit("上传中 . . .")
-                await context.client.send_file(context.chat_id,filename)
+                    await context.client.send_file(
+                    context.chat_id,
+                    filename,
+                    reply_to=None,
+                    caption=None
+                    )
+                try:
+                    remove(filename)
+                except:
+                    pass
                 status = True
                 break #成功了就赶紧结束啦！
         except:
-            try:
-                remove(filename)
-            except:
-                pass
             continue
-    try:
-        remove(filename)
-    except:
-        pass
-    try:
-        await context.delete()
-    except:
-        pass
     if not status:
-        try:
-            remove(filename)
-        except:
-            pass
-        try:
-            await context.delete()
-        except:
-            pass
-        await context.client.send_message(context.chat_id,"出错了呜呜呜 ~ 试了好多好多次都无法访问到服务器 。")
+        await context.edit("出错了呜呜呜 ~ 试了好多好多次都无法访问到 API 服务器 。")
+        sleep(2)
+    await context.delete()
